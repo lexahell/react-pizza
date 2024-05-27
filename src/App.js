cc16a91469c0545469ea7dab39e3e03cc7884bb2
aleksey-grigorev <aleksey-grigorev7@mail.ru>
2024-05-27 21:33:39 +0300

    feat: Add pizza request from server

--- src/App.js
+++ src/App.js
@@ -5,9 +5,16 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
-import pizzas from './assets/pizzas.json';
-
 function App() {
+  const [items, setItems] = React.useState([]);
+  React.useEffect(() => {
+    fetch('https://66506f00ec9b4a4a60320d7d.mockapi.io/items')
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+      });
+  }, []);
+
   return (
     <div className='wrapper'>
       <Header />
@@ -19,7 +26,7 @@ function App() {
           </div>
           <h2 className='content__title'>Все пиццы</h2>
           <div className='content__items'>
-            {pizzas.map((obj) => {
+            {items.map((obj) => {
               return <PizzaBlock key={obj.id} {...obj} />;
             })}
           </div>
