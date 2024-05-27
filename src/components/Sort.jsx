72d4be31a9cfc9d53a66793cf7f492f8f8edc120
aleksey-grigorev <aleksey-grigorev7@mail.ru>
2024-05-27 20:55:15 +0300

    feat: Add display of sorting popup window

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,15 @@
+import React from 'react';
+
 function Sort() {
+  const [open, setOpen] = React.useState(false);
+  const [selected, setSelected] = React.useState(0);
+  const list = ['популярности', 'цене', 'алфавиту'];
+
+  const onClickListItem = (index) => {
+    setSelected(index);
+    setOpen(false);
+  };
+
   return (
     <div className='sort'>
       <div className='sort__label'>
@@ -15,15 +26,23 @@ function Sort() {
           />
         </svg>
         <b>Сортировка по:</b>
-        <span>популярности</span>
+        <span onClick={() => setOpen(!open)}>{list[selected]}</span>
       </div>
-      <div className='sort__popup'>
-        <ul>
-          <li className='active'>популярности</li>
-          <li>цене</li>
-          <li>алфавиту</li>
-        </ul>
-      </div>
+      {open && (
+        <div className='sort__popup'>
+          <ul>
+            {list.map((name, index) => (
+              <li
+                key={name}
+                onClick={() => onClickListItem(index)}
+                className={selected === index ? 'active' : ''}
+              >
+                {name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
